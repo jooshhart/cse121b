201568916f5dd597fc24cfae707dc1c3c06bb626
jooshhart <jooshhart@gmail.com>
2023-10-24 10:40:14 -0600

    y

--- scripts/project.js
+++ scripts/project.js
@@ -1,6 +1,7 @@
 const verdictElement = document.querySelector("#verdict");
 let verdictList = [];
 var income = 0;
+
 function myFunction ()
 {
     var rent = parseFloat(document.getElementById("rent").value);
